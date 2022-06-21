9d212eff6a5527030e3a8326a8563518fbba852f
nwade-godaddy <80975008+nwade-godaddy@users.noreply.github.com>
2022-06-21 07:31:17 -0500

    Add common metrics library (#22)

--- service/rest/__init__.py
+++ service/rest/__init__.py
@@ -1,7 +1,6 @@
 import os
 
-from dcustructuredloggingflask.flasklogger import add_request_logging
-from elasticapm.contrib.flask import ElasticAPM
+from csetutils.flask import instrument
 from flask import Flask
 
 from service.cache.redis_cache import RedisCache
@@ -11,15 +10,12 @@ from .api import api as ns1
 
 def create_app(config):
     app = Flask(__name__)
-    apm = ElasticAPM()
-    apm.init_app(app, service_name='auto-abuse-id', debug=True, environment=os.getenv('sysenv', 'dev'))
     app.config.SWAGGER_UI_JSONEDITOR = True
     app.config.SWAGGER_UI_DOC_EXPANSION = 'list'
     app.config['token_authority'] = config.TOKEN_AUTHORITY
     app.config['cache'] = RedisCache(config.CACHE_SERVICE)
     app.register_blueprint(ns1)
-
-    add_request_logging(app, 'auto-abuse-api', sso=config.TOKEN_AUTHORITY, excluded_paths=[
+    instrument(app, 'auto-abuse-id', env=os.getenv('sysenv', 'dev'), sso=config.TOKEN_AUTHORITY, excluded_paths=[
         '/doc/',
         '/classify/health'
     ], min_status_code=300)
